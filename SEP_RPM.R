ff14e5b1983fb54faffcfc49e9b24f7098d067a0
Giovanni Rebaudo <rebaudo.giovanni@gmail.com>
2023-11-17 16:28:19 +0100

    upd

--- SEP_RPM.R
+++ SEP_RPM.R
@@ -237,289 +237,50 @@ na_in_rows=sapply(1:dim(data2)[1],function(i) sum(data2[i,]==0))
 data3=data.frame(data2[na_in_rows!=38,]) #remove all na rows
 data=data3[,2:39]
 
-cluster_per_obs <- rep(point_SJ,nrow(data))
+
+# This plot is made adapting the code in Fradenti Github rep CommonAtomModel
+cluster_per_obs <- rep(point_SJ, rep(nrow(data), ncol(data)))
 LL <- apply(data,2,function(x) cumsum(sort((x),decreasing = T)) /sum((x)) )
-LL <- cbind(LL,point_SJ)
+dim(LL)
 LL2 <- reshape2::melt(LL)
 LL2 <- cbind(LL2,cluster_per_obs)
-LL2 <- as_tibble(LL2) %>% mutate(cluster_per_obs=paste0("SC-",cluster_per_obs))
-ggplot(LL2)+
+LL2 <- as_tibble(LL2) %>% mutate(cluster_per_obs=paste0("",cluster_per_obs))
+P = ggplot(LL2)+
   geom_hline(yintercept = c(0,1),lty=3)+
   geom_line(aes(x=Var1,y=value,group=Var2,col=as.factor(cluster_per_obs)),
-            lwd=.5,alpha=.2)+
+            lwd=.5, alpha=.2)+
   geom_point(aes(x=Var1,y=value,group=Var2,col=as.factor(cluster_per_obs)),
-             lwd=1,alpha=.9)+
-  theme_bw()+scale_color_manual("Subject\nCluster", 
-                                values = c(2,4,1,6,5)) + ylim(0,1)+
-  ggtitle("CRF of microbiome subpopulations") +
-  xlab("Taxa sorted by count") + 
-  ylab("Cumulative Relative Frequncy")
+             alpha=.9)+
+  theme_bw()+scale_color_manual("Subject\nCluster",
+                                values = c(2,4,1,6,5)) + ylim(0, 1)+
+  xlab("Taxa sorted by count")+ylab("Cumulative Relative Frequncy")
+ggsave(plot=P, file="Image/Sjclustergg2.pdf", height = 2.5, width = 4)    
 
+#### Plot heatmaps
 
-
-
-
-
-##### check plotting log-likelihood, cluster y mean and weights
-par(mar=c(2,2,1,1))
-plot(ll[-1],type='l')
-
-# par(mar=c(2,2,1,1))
-# par(mfrow=c(3,1))
-# hist(mki[T,1,])
-# hist(mki[T,2,])
-# hist(mki[T,3,])
-# par(mfrow=c(1,1))
-
-# plot( rowMeans(y[, Sj0==1]), type='l')
-# lines(rowMeans(y[, Sj0==2]), col='red')
-# lines(rowMeans(y[, Sj0==4]), col='blue')
-
-y1   = y[order(rowSums(y),decreasing = T),]
-y_s1 = as.matrix(y1[, point_SJ==1])
-y_s2 = as.matrix(y1[, point_SJ==2])
-y_s3 = as.matrix(y1[, point_SJ==3])
-# 
-# par(mfrow=c(1,1))
-# plot(rowMeans(y_s1),type='l',xlab='OTU 1-120',ylab='Abundance',cex.lab=0.6,cex.axis=0.6)
-# lines(rowMeans(y_s2),type='l',col='red')
-# lines(rowMeans(y_s3),type='l',col='blue')
-
-pdf_s1=rowSums(y_s1)/sum(rowSums(y_s1))
-pdf_s2=rowSums(y_s2)/sum(rowSums(y_s2))
-pdf_s3=rowSums(y_s3)/sum(rowSums(y_s3))
-
-# plot(c(0,cumsum(pdf_s1)),type='l',ylim=c(0,1),xlim=c(0,120),xlab='OTU',ylab="CDF of Relative Abundance")
-# lines(c(0,cumsum(pdf_s2)),type='l',col='red')
-# lines(c(0,cumsum(pdf_s3)),type='l',col='blue')
-
-### plot according to country label
-# data_country=pivot_wider(data[,c(1:2,6)],names_from = Sample,values_from = nationality)
+# data_country=pivot_wider(data[,c(1:2,6)], 
+#                          names_from = Sample, values_from = nationality)
 # data_country=data_country[1,2:dim(data_country)[2]]
-
-# y_af=as.matrix(y1[,data_country=='AFR'])
-# y_am=as.matrix(y1[,data_country=='AAM'])
-
-# as.matrix(data_country[point_SJ==1])[1,]
-# as.matrix(data_country[point_SJ==2])[1,]
-# as.matrix(data_country[point_SJ==3])[1,]
-
-# pdf_af=rowSums(y_af)/sum(rowSums(y_af))
-# pdf_am=rowSums(y_am)/sum(rowSums(y_am))
 # 
-# plot(c(0,cumsum(pdf_af)),type='l',ylim=c(0,1),xlim=c(0,120),xlab='OTU',ylab="CDF of Relative Abundance")
-# lines(c(0,cumsum(pdf_am)),type='l',col='red')
-
-
-# mik[1,order(rowSums(y),decreasing = T)]
-# mik[2,order(rowSums(y),decreasing = T)]
-# mik[3,order(rowSums(y),decreasing = T)]
-# w[T,1,]
-# w[T,2,]
-# w[T,3,]
-
-# w1_ave=sapply(1:L,function(i) w_store[T,1,i]/sum(mki[T,1,]==i))
-# mik1_plot=w1_ave[mki[T,1,order(rowSums(y),decreasing = T)]]
 # 
-# w2_ave=sapply(1:L,function(i) w_store[T,2,i]/sum(mki[T,2,]==i))
-# mik2_plot=w2_ave[mki[T,2,order(rowSums(y),decreasing = T)]]
+# y[order(mik_star[1,]), point_SJ==1]
+# y[order(mik_star[2,]), point_SJ==2]
+# y[order(mik_star[3,]), point_SJ==3]
 # 
-# w3_ave=sapply(1:L,function(i) w_store[T,3,i]/sum(mki[T,3,]==i))
-# mik3_plot=w3_ave[mki[T,3,order(rowSums(y),decreasing = T)]]
-# 
-# plot(cumsum(mik1_plot),type='l')
-# lines(cumsum(mik2_plot),type='l',col='red')
-# lines(cumsum(mik3_plot),type='l',col='blue')
+# od1=unlist(sapply(1:8,function (i) which(mik_star[1,]==i)))
+# od2=unlist(sapply(1:8,function (i) which(mik_star[2,]==i)))
+# od3=unlist(sapply(1:8,function (i) which(mik_star[3,]==i)))
 
+point_SJ = salso::salso(Sj, nRuns = 100, maxZealousAttempts = 100, loss=VI())
 
-###### Do David Dahl 
-# idx=1:T
-# idx2=idx[idx>7500]
-# TT=length(idx2)
+salso::salso(Sj, nRuns = 100, maxZealousAttempts = 100, loss=VI())
 
-#one_pos means positions of 1s, it is to save space for the binary matrix
-one_pos=list()
-
-TT = niter
-for(tt in 1:TT){
-  ii=idx2[tt]
-  s_temp=Sj[ii]
-  cl_temp=unique(s_temp)
-  s_mat_temp=matrix(0,n,n)
-  for(ss in cl_temp){
-    s_mat_temp[which(s_temp==ss),which(s_temp==ss)]=1
-  }
-  svec=s_mat_temp[lower.tri(s_mat_temp,diag = F)]
-  one_pos[[tt]]=which(svec==1)
-  print(tt)
-}
-
-hem_dist=rep(0, TT)
-
-hem_dist_mat=matrix(0,TT,TT)
-for(ii in 1:TT){
-  for(jj in ii:TT){
-    print(c(ii,jj))
-    s1=rep(0,n*(n-1)/2)
-    s2=rep(0,n*(n-1)/2)
-    s1[one_pos[[ii]]]=1
-    s2[one_pos[[jj]]]=1
-    hem_dist_mat[ii,jj]=sum(s1!=s2)*2
-  }
-}
-
-hem_dist_mat2=t(hem_dist_mat)+hem_dist_mat
-hem_dist = rowMeans(hem_dist_mat2)
-which.min(hem_dist)
-
-### from the cluster assignmebt and iteration picked from result above, 
-### do David Dahl for the nested partitions
-idx_star=idx2[which.min(hem_dist)]
-s_star=Sj[idx_star,]
-
-idx_star_all=idx[sapply(1:T, function(i) sum(Sj[i,]==s_star)==n)]
-idx_star_m=idx_star_all[idx_star_all>8000]
-TT=length(idx_star_m)
-one_pos_m1=list()
-one_pos_m2=list()
-one_pos_m3=list()
-
-for(tt in 1:TT){
-  ii=idx_star_m[tt]
-  s_temp=mki[ii,1,]
-  cl_temp=unique(s_temp)
-  s_mat_temp=matrix(0,B,B)
-  for(ss in cl_temp){
-    s_mat_temp[which(s_temp==ss),which(s_temp==ss)]=1
-  }
-  svec=s_mat_temp[lower.tri(s_mat_temp,diag = F)]
-  one_pos_m1[[tt]]=which(svec==1)
-  
-  s_temp=mki[ii,2,]
-  cl_temp=unique(s_temp)
-  s_mat_temp=matrix(0,B,B)
-  for(ss in cl_temp){
-    s_mat_temp[which(s_temp==ss),which(s_temp==ss)]=1
-  }
-  svec=s_mat_temp[lower.tri(s_mat_temp,diag = F)]
-  one_pos_m2[[tt]]=which(svec==1)
-  
-  s_temp=mki[ii,3,]
-  cl_temp=unique(s_temp)
-  s_mat_temp=matrix(0,B,B)
-  for(ss in cl_temp){
-    s_mat_temp[which(s_temp==ss),which(s_temp==ss)]=1
-  }
-  svec=s_mat_temp[lower.tri(s_mat_temp,diag = F)]
-  one_pos_m3[[tt]]=which(svec==1)
-  
-  print(tt)
-  
-}
-
-
-hem_dist_mat_m1=matrix(0,TT,TT)
-hem_dist_mat_m2=matrix(0,TT,TT)
-hem_dist_mat_m3=matrix(0,TT,TT)
-
-for(ii in 1:TT){
-  for(jj in ii:TT){
-    print(c(ii,jj))
-    s1=rep(0,B*(B-1)/2)
-    s2=rep(0,B*(B-1)/2)
-    s1[one_pos_m1[[ii]]]=1
-    s2[one_pos_m1[[jj]]]=1
-    #hem_dist[ii]=hem_dist[ii]+sum(s1!=s2)
-    hem_dist_mat_m1[ii,jj]=sum(s1!=s2)*2
-    
-    s1=rep(0,B*(B-1)/2)
-    s2=rep(0,B*(B-1)/2)
-    s1[one_pos_m2[[ii]]]=1
-    s2[one_pos_m2[[jj]]]=1
-    #hem_dist[ii]=hem_dist[ii]+sum(s1!=s2)
-    hem_dist_mat_m2[ii,jj]=sum(s1!=s2)*2
-    
-    s1=rep(0,B*(B-1)/2)
-    s2=rep(0,B*(B-1)/2)
-    s1[one_pos_m3[[ii]]]=1
-    s2[one_pos_m3[[jj]]]=1
-    #hem_dist[ii]=hem_dist[ii]+sum(s1!=s2)
-    hem_dist_mat_m3[ii,jj]=sum(s1!=s2)*2
-  }
-  #hem_dist[ii]=hem_dist[ii]/T
-}
-
-hem_dist_mat2_m1=t(hem_dist_mat_m1)+hem_dist_mat_m1
-hem_dist_m1 = rowMeans(hem_dist_mat2_m1)
-which.min(hem_dist_m1)
-
-hem_dist_mat2_m2=t(hem_dist_mat_m2)+hem_dist_mat_m2
-hem_dist_m2 = rowMeans(hem_dist_mat2_m2)
-which.min(hem_dist_m2)
-
-hem_dist_mat2_m3=t(hem_dist_mat_m3)+hem_dist_mat_m3
-hem_dist_m3 = rowMeans(hem_dist_mat2_m3)
-which.min(hem_dist_m3)
-
-
-####### form the cluster assignment, plot pdfs
-mik_star=matrix(0,3,B)
-mik_star[1,]=mki[idx_star_m[which.min(hem_dist_m1)],1,]
-mik_star[2,]=mki[idx_star_m[which.min(hem_dist_m2)],2,]
-mik_star[3,]=mki[idx_star_m[which.min(hem_dist_m3)],3,]
-
-
-y_s1=as.matrix(y1[,s_star==1])
-y_s2=as.matrix(y1[,s_star==2])
-y_s3=as.matrix(y1[,s_star==3])
-
-pdf_s1=rowSums(y_s1)/sum(rowSums(y_s1))
-pdf_s2=rowSums(y_s2)/sum(rowSums(y_s2))
-pdf_s3=rowSums(y_s3)/sum(rowSums(y_s3))
-
-pdf('Sjcluster.pdf',width=4,height=3)
-plot(c(0,cumsum(pdf_s1)),type='l',ylim=c(0,1),xlim=c(0,120),xlab='OTU',ylab="CDF of Relative Abundance")
-lines(c(0,cumsum(pdf_s2)),type='l',col='red')
-lines(c(0,cumsum(pdf_s3)),type='l',col='blue')
-legend("bottomright",lty=c(1,1,1),col=c("black",'red','blue'),legend=c("cluster 1","cluster 2","cluster 3"),bty = 'n',cex=0.5)
-dev.off()
-
-library(ggplot2)
-ggplot()+
-  geom_line(aes(0:119,c(0,cumsum(pdf_s1)),col='cluster1'))+
-  geom_line(aes(0:119,c(0,cumsum(pdf_s2)),col='cluster2'))+
-  geom_line(aes(0:119,c(0,cumsum(pdf_s3)),col='cluster3'))+
-  labs(x='OTU',y="Cumulative Relative Frequencies")+
-  theme(legend.position = 'bottom')
-
-ggsave("Sjclustergg.pdf",width=5,height=3.5,units = 'in')
-
-for(j in 1:dim(y_s1)[2]){
-  pdf_s1=y_s1[,j]/sum(y_s1[,j])
-  points(c(0,cumsum(pdf_s1)),pch=1,cex=0.1)
-}
-
-
-#### plot heatmaps
-
-data_country=pivot_wider(data[,c(1:2,6)],names_from = Sample,values_from = nationality)
-data_country=data_country[1,2:dim(data_country)[2]]
-
-
-y[order(mik_star[1,]),s_star==1]
-y[order(mik_star[2,]),s_star==2]
-y[order(mik_star[3,]),s_star==3]
-
-od1=unlist(sapply(1:8,function (i) which(mik_star[1,]==i)))
-od2=unlist(sapply(1:8,function (i) which(mik_star[2,]==i)))
-od3=unlist(sapply(1:8,function (i) which(mik_star[3,]==i)))
-
-pdf("heatmap.pdf",width=8,height=3)
+# pdf("heatmap.pdf",width=8,height=3)
 par(mfrow=c(1,3))
-heatmap(y[order(mik_star[1,]),s_star==1],Colv = NA, Rowv = NA)
-heatmap(y[order(mik_star[2,]),s_star==2],Colv = NA, Rowv = NA)
-heatmap(y[order(mik_star[3,]),s_star==3],Colv = NA, Rowv = NA)
-dev.off()
+heatmap(data[order(mki[1,]), point_SJ==1], Colv = NA, Rowv = NA)
+heatmap(data[order(mki[2,]), point_SJ==2], Colv = NA, Rowv = NA)
+heatmap(data[order(mki[3,]), point_SJ==3], Colv = NA, Rowv = NA)
+# dev.off()
 
 pdf("heatmap1.pdf",width=4,height=3)
 heatmap(y[od1,s_star==1],Colv = NA, Rowv = NA)
@@ -540,8 +301,9 @@ heatmap(y[,s_star==3],Colv = NA, Rowv = NA)
 
 
 charOTU <- function(s1,s2){
-  idx=sort(abs(rowMeans(y[,s_star==s1])-rowMeans(y[,s_star==s2])),decreasing=TRUE,index.return=T)$ix
-  names=c(rownames(y)[idx==1],rownames(y)[idx==2],rownames(y)[idx==3])
+  idx   <- sort(abs(rowMeans(y[,s_star==s1])-rowMeans(y[,s_star==s2])), 
+           decreasing=TRUE, index.return=T)$ix
+  names <- c(rownames(y)[idx==1], rownames(y)[idx==2], rownames(y)[idx==3])
   return(names)
 }
 charOTU(1,2)
